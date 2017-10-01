bd8fc45a8bc8525b50685a2c29c50b86dd910e95
Kawazu <kawazugekogeko@gmail.com>
2017-10-01 06:57:39 +0000

    Finish routes_r2

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.5'
 gem 'bootstrap-sass', '3.3.1'
 gem 'sprockets', '2.11.0'
 # Use sqlite3 as the database for Active Record
+gem 'devise', '3.4.1'
 gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921131911) do
+ActiveRecord::Schema.define(version: 20170929140318) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.string   "detail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
